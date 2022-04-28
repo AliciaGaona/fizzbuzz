9b1854021b37a160676dc06ec04680dfba1b8ce5
AliciaGaona <aligaovela@gmail.com>
2022-04-27 19:00:44 -0500

    5.Refactor de la lectura del archivo.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const fs = require("fs");
-
+//const fs = require("fs");
 // Part 1 Read json file ===========================
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+//const rawdata = fs.readFileSync("explorers.json"); //readFileSync sirve para leer archivo
+//const explorers = JSON.parse(rawdata); // lo que lee lo gurada en un JSON en explorers
+
+const Reader = require('./lib/utils/Reader.js');
+const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+console.log(explorers)
+
 
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+console.log(explorersInNode.length)//filtro de los explorers que tienen mission "node"
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
 const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+console.log(usernamesInNode)
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
@@ -61,3 +65,5 @@ const assignFizzBuzzTrick = function(explorer){
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+
+

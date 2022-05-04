a4f51250a47279e1e0eb8351ef237e0e8dd0c7fa
AliciaGaona <aligaovela@gmail.com>
2022-05-04 00:09:29 -0500

    Se crea prueba unitaria se hace pasar la prueba unitaria de clase Reader

--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -1,7 +1,6 @@
 const Reader= require('./../../lib/utils/Reader')
 
-
     test("1. leer archivo Json que no existe", ()=>{
-     const explorers = Reader.readJsonFile("explorers1.json"); 
+     const explorers = Reader.readJsonFile("explorers.json"); 
      expect(explorers).toBe(explorers)
      })

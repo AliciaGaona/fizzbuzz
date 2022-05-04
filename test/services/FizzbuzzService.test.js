557c9f0855f371f28536fd70fec8247c5ab85d23
AliciaGaona <aligaovela@gmail.com>
2022-05-04 01:05:40 -0500

    Se agregan pruebas unitarias para los tres casos de las validaciones de la clase FizzbuzzService

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -1,8 +1,15 @@
 const FizzbuzzService= require('./../../lib/services/FizzbuzzService')
 
+test("1. Validacion de explorers applyValidationInExplorer score divisible entre 3&&5", ()=>{
+    const explorer = FizzbuzzService.applyValidationInExplorer("Woopa15")   
+    expect(explorer).toBe({name: "Woopa6", score: 15, trick:"FIZZBUZZ"})
+})
 
-test("1. Validacion de explorers applyValidationInExplorer", ()=>{
-    const explorers = [{name: "Woopa6", score: 1}]
-    const explorersList= FizzbuzzService.applyValidationInExplorer(explorers)
-    expect(explorers).toBe(explorers.name, explorers.score)
+test("2. Validacion de explorers applyValidationInExplorer score divisible entre 3", ()=>{
+    const explorer = FizzbuzzService.applyValidationInExplorer("Woopa6")   
+    expect(explorer).toBe({name: "Woopa6", score: 6, trick:"FIZZ"})
+})
+test("3. Validacion de explorers applyValidationInExplorer score divisible entre 5", ()=>{
+    const explorer = FizzbuzzService.applyValidationInExplorer("Woopa5")   
+    expect(explorer).toBe({name: "Woopa6", score: 5, trick:"BUZZ"})
 })

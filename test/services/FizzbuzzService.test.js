cdf8f5edbfa219199633f3b5076bdc4f37d540aa
AliciaGaona <aligaovela@gmail.com>
2022-05-04 01:39:43 -0500

    Se intenta hacer fallar la prueba unitaria de Fizzbuzz, pero no encuentra la informacion

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -6,8 +6,9 @@ test("1. Validacion de explorers applyValidationInExplorer score divisible entre
 })
 
 test("2. Validacion de explorers applyValidationInExplorer score divisible entre 3", ()=>{
-    const explorer = FizzbuzzService.applyValidationInExplorer("Woopa6")   
-    expect(explorer).toBe({name: "Woopa6", score: 6, trick:"FIZZ"})
+    const explorer = FizzbuzzService.applyValidationInExplorer("Woopa6") 
+    const trick="FIZZ"  
+    expect(explorer.trick).toBe(trick)
 })
 test("3. Validacion de explorers applyValidationInExplorer score divisible entre 5", ()=>{
     const explorer = FizzbuzzService.applyValidationInExplorer("Woopa5")   

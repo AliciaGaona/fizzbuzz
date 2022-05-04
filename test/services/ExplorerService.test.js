eb4036f0994e726f8aa476a2651ec33eaa79e40f
AliciaGaona <aligaovela@gmail.com>
2022-05-04 00:17:00 -0500

    Prueba unitaria para requerimiento 1 de ExplorerService, se hace que pase

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -5,9 +5,5 @@ const ExplorerService= require('./../../lib/services/ExplorerService')
         const explorersInNode = ExplorerService.filterByMission(explorers, "node")
         expect(explorersInNode.length).toBe(1)
     })
-    test("1.1 Crear filtro de explorers en una mission qu eno existe", ()=>{
-        const explorers = [{mission: "holanoExisto"}]
-        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
-        expect(explorersInNode.length).toBe(1)
-    })
+
 

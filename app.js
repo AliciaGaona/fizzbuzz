695ad1a1fbc3fba65771b3934dea25c25f9e08cf
AliciaGaona <aligaovela@gmail.com>
2022-05-05 03:06:17 -0500

    Se quitan carpetas que no se usan, acomodando explorers.json oara que fs pueda encontrarlo

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const ExplorerService=require('./lib/services/ExplorerService')
 const FizzbuzzService=require('./lib/services/FizzbuzzService')
-const Reader = require('./lib/utils/Reader.js');
+const Reader = require('./lib/utils/Reader');
+
 const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
-//console.log(explorers)
+console.log(explorers)
 
 
 // Aplicación del ExplorerService sobre la lista de explorers

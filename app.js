78afb84589e24cd15ed5078e1fae59c5b31b6c00
AliciaGaona <aligaovela@gmail.com>
2022-05-05 02:05:15 -0500

    Se agrega require de clases Servicio Explorer y Fizzbuzz

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const ExplorerService=require('./lib/services/ExplorerService')
+const FizzbuzzService=require('./lib/services/FizzbuzzService')
 const Reader = require('./lib/utils/Reader.js');
 const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
 console.log(explorers)

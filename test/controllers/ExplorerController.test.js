92bef8a1e916ec95751392b4735b313ad2910c64
AliciaGaona <aligaovela@gmail.com>
2022-05-05 20:34:18 -0500

    Se valida que falle primera prueba unitaria para getExplorersByMission, y se arregla paar que pase

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -3,9 +3,7 @@ const ExplorerController=require('../../lib/controllers/ExplorerController')
 
 describe("Test service ExplorerController", ()=>{
     test("1.Probando metodo static getExplorersByMission, que recibe lista de explorers", ()=>{
-        const getExplorers=ExplorerController.getExplorersByMission("mission")
-        expect(explorers).toBeDefined()
+        const getExplorers=ExplorerController.getExplorersByMission("node")
+        expect(getExplorers).toBeDefined()
     })
-
-
 })

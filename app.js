cd52a325dd07df376e30c062d9cc6d7bdf50bbc8
AliciaGaona <aligaovela@gmail.com>
2022-05-10 02:11:18 -0500

    Se quita código de  Parse

--- app.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ debug: process.env.TOKEN })
-
-
-const dotenv = require('dotenv')
-const buf = Buffer.from('TOKEN=token')
-const config = dotenv.parse(TOKEN) // will return an object
-console.log(typeof config, config) // object { BASIC : 'basic' }

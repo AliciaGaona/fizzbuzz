0d121d8442428f99f26ffbec7daa00f269aa6222
AliciaGaona <aligaovela@gmail.com>
2022-05-10 02:10:12 -0500

    Se prueba agregando Parse

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-require('dotenv').config()
+require('dotenv').config({ debug: process.env.TOKEN })
 
-if(!process.env.TOKENBOTTELEGRAM){
-  throw new Error('No hay configuración con Api Key y con Token')
-}
+
+const dotenv = require('dotenv')
+const buf = Buffer.from('TOKEN=token')
+const config = dotenv.parse(TOKEN) // will return an object
+console.log(typeof config, config) // object { BASIC : 'basic' }

8532a1a4cd2937cb8590fe31d3994f98994a4621
AliciaGaona <aligaovela@gmail.com>
2022-05-10 23:18:47 -0500

    Se prueba en app.js el funcionamiento del método de validación de fizzbuzz del controller

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+
+const ExplorerController = require('./lib/controllers/ExplorerController');
 require('dotenv').config()
 console.log(process.env.TOKEN) 
+
+
+const fiz =ExplorerController.applyValidationInNumber(3);
+console.log(fiz)
